8eb3eec95ab6ad28116177feb8d54f434eb7af95
mekakano <38857612+mekakano@users.noreply.github.com>
2024-09-27 18:53:53 +0700

    fixing untuk leo ganteng

--- src/main/java/com/gealocker/be/gealockerbe/GealockerbeApplication.java
+++ src/main/java/com/gealocker/be/gealockerbe/GealockerbeApplication.java
@@ -2,9 +2,10 @@ package com.gealocker.be.gealockerbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GealockerbeApplication {
+public class GealockerbeApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GealockerbeApplication.class, args);
--- src/main/java/com/gealocker/be/gealockerbe/config/JdbcProperties.java
+++ src/main/java/com/gealocker/be/gealockerbe/config/JdbcProperties.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @Setter
 @Configuration
 public class JdbcProperties {
-    private String JDBC_DRIVER = "org.postgresql.Driver";
-    private String JDBC_URI = "jdbc:postgresql://localhost:9721/linovdb";
+	private String JDBC_DRIVER = "org.postgresql.Driver";
+    private String JDBC_URI = "jdbc:postgresql://128.199.218.194:9721/linovdb";
     private String JDBC_USERNAME = "linovdb";
     private String JDBC_PASSWORD = "linov";
 }
